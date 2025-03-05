c681e00acf287d1c1dfdf0abed3747ba81221fd5
javascriptru <67475495+jsru-1@users.noreply.github.com>
2025-03-05 23:46:46 +0300

    Added task Сумма двух чисел

---
+++ 00-intro/10-sum/__tests__/sum.test.ts
@@ -0,0 +1,20 @@
+import { describe, it, expect } from 'vitest'
+import { sum } from '@/sum.js'
+
+describe('intro/sum', () => {
+  describe('Функция sum', () => {
+    it('Функция sum должна быть определена', () => {
+      expect(sum).toBeDefined()
+    })
+
+    it.each`
+      a     | b     | expected
+      ${1}  | ${1}  | ${2}
+      ${10} | ${20} | ${30}
+      ${0}  | ${0}  | ${0}
+      ${-2} | ${-1} | ${-3}
+    `('Функция sum должна получать $a + $b = $expected', ({a, b, expected}) => {
+      expect(sum(a, b)).toBe(expected)
+    })
+  })
+})
---
+++ 00-intro/10-sum/script.js
@@ -0,0 +1,18 @@
+import { sum } from './sum.js'
+
+const a = document.querySelector('input[name="num-a"]')
+const b = document.querySelector('input[name="num-b"]')
+const result = document.querySelector('output[name="result"]')
+
+function calculate() {
+  try {
+    result.value = sum(a.valueAsNumber, b.valueAsNumber)
+  } catch (e) {
+    result.value = e.message
+  }
+}
+
+a.addEventListener('input', calculate)
+b.addEventListener('input', calculate)
+
+document.querySelector('form').addEventListener('submit', (event) => event.preventDefault())
